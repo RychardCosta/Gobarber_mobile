98e5671e5f3c485ba860f813db61fc8a77f18f76
Rychard Costa <rychard@yahoo.com>
2021-03-14 12:09:24 -0300

    Add feature of image upload

--- src/routes/users.routes.ts
+++ src/routes/users.routes.ts
@@ -1,8 +1,13 @@
 import { Router } from 'express';
+import multer from 'multer';
 
 import CreateUserService from '../services/CreateUserService';
+import uploadConfig from '../config/upload';
+import ensureAuthenticate from '../middlewires/ensureAuthenticate';
+import UpdateUserAvatarService from '../services/UpdateUserAvatarService';
 
 const usersRoutes = Router();
+const upload = multer(uploadConfig);
 
 usersRoutes.post('/', async (request, response) => {
   try {
@@ -20,5 +25,27 @@ usersRoutes.post('/', async (request, response) => {
     return response.status(400).json({ error: err.message });
   }
 });
+
+usersRoutes.patch(
+  '/',
+  ensureAuthenticate,
+  upload.single('avatar'),
+  async (request, response) => {
+    try {
+      const updateUserAvatar = new UpdateUserAvatarService();
+
+      const user = await updateUserAvatar.execute({
+        user_id: request.user.id,
+        avatarFileName: request.file.filename,
+      });
+
+      delete user.password;
+
+      return response.json(user);
+    } catch (err) {
+      return response.status(400).json({ error: err.message });
+    }
+  },
+);
 
 export default usersRoutes;
