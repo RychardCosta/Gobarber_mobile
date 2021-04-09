5fba0b2c9e8549e893854cf62f50c74d1f257bb8
RychardCosta <55329339+RychardCosta@users.noreply.github.com>
2021-04-09 18:07:43 -0300

    Delete frontend directory

--- frontend/src/pages/Dashboard/styles.ts
+++
@@ -1,8 +0,0 @@
-import styled from 'styled-components';
-
-export const Header = styled.header`
-  display: flex;
-  justify-content: space-between;
-  padding: 25px;
-`;
-export const Container = styled.div``;
